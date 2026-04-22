f8055f73c4b3c8c52ae82bf88e7bbd6ef36c1926
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:54 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Dictionaries.IHeader.Tests/IHeaderDictionaryExtensionTests.cs
+++ test/Soenneker.Extensions.Dictionaries.IHeader.Tests/IHeaderDictionaryExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Dictionaries.IHeader.Tests;
 
 public sealed class IHeaderDictionaryExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
